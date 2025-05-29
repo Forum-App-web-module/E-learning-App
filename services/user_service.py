98cd22c2dd75d1022dee3d17a5f909e78805f892
birkata <birkata@yahoo.com>
2025-05-29 17:13:16 +0300

    partial refactor of account insertion

--- services/user_service.py
+++ services/user_service.py
@@ -17,11 +17,11 @@ async def create_account(data: RegisterData, hashed_password: str):
     if hasattr(data, "mobile") and hasattr(data, "linked_in_url"):
         # Converting if data is not yet a TeacherRegisterData
         if not isinstance(data, TeacherRegisterData):
-            data = TeacherRegisterData(**data.dict())
+            data = TeacherRegisterData(**data.model_dump())
     else:
         # Same for StudentRegisterData instance
         if not isinstance(data, StudentRegisterData):
-            data = StudentRegisterData(**data.dict())
+            data = StudentRegisterData(**data.model_dump())
 
     role, user_id = await insert_user(data, hashed_password)
     return role, user_id
