1da595888cf3372700055dc2e7fe8257d7d378ef
dilyanab <bozhinova.dilyana@gmail.com>
2025-05-31 12:14:00 +0300

    adding UniqueViolationError exception in create_course

--- services/course_service.py
+++ services/course_service.py
@@ -1,6 +1,8 @@
 from repositories.course_repo import read_all_courses, read_course_by_id, insert_course, update_course_data
 from common.responses import Unauthorized, NotFound
 from data.models import CourseCreate, CourseUpdate
+from asyncpg.exceptions import UniqueViolationError
+from fastapi.exceptions import HTTPException
 
 async def get_course_by_id_service(id: int):
     return await read_course_by_id(id)
@@ -9,7 +11,10 @@ async def get_all_courses_service():
     return await read_all_courses()
 
 async def create_course_service(course_data: CourseCreate):
-    return await insert_course(course_data)
+    try:
+        return await insert_course(course_data)
+    except UniqueViolationError:
+        raise HTTPException(status_code=400, detail="Course with this title already axists")
 
 async def update_course_service(id: int, updates: CourseUpdate):
     return await update_course_data(id, updates)
