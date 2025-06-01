7ff02c42d4fb2e06b165f4a5451fba431a29bc85
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-06-01 22:45:31 +0300

    subscription fucntions

--- services/subscription_service.py
+++ services/subscription_service.py
@@ -1,9 +1,17 @@
 from data.models import Subscription
 from data import database
+from repositories.student_repo import repo_is_subscribed, repo_subscribe
+from asyncpg import UniqueViolationError
+from fastapi import HTTPException
+
+async def is_subscribed(student_id):
+    await repo_is_subscribed(student_id)
+
+async def subscribe(student_id):
+    try: 
+        await repo_subscribe(student_id)
+    except UniqueViolationError:
+        raise HTTPException(status_code=400, detail="Student is already subscribed for premium account!")
 
 
-def is_subscribed():
-    pass
-
-def subscribe():
-    pass
+    
