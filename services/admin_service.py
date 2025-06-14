11fb8abf99719388ecc84b3b0d2fd43dab292b3d
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-06-14 11:52:28 +0300

    adding activation + deactivation admin endpoint

--- services/admin_service.py
+++ services/admin_service.py
@@ -1,10 +1,13 @@
 
-from repositories.admin_repo import approve_teacher_repo, delete_course_repo
+from repositories.admin_repo import change_account_state_repo, delete_course_repo
 from repositories.teacher_repo import report_enrolled_students
 from repositories.course_repo import read_course_by_id
+from data.models import Action_UserRole, Action
 
-async def approve_teacher(teacher_id):
-    return await approve_teacher_repo(teacher_id)
+async def change_account_state(role: Action_UserRole, action: Action, user_id: int):
+    change = await change_account_state_repo(role, action, user_id)
+    return change if change else None
+
 
 async def delete_course_service(course_id: int):
     course_data = await read_course_by_id(course_id)
